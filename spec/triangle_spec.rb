c72d6cb06d5eebbcc44c77122f45118da0369afd
zukkun <zukkun@gmail.com>
2014-10-19 15:10:42 +0900

    Add rspec test case

--- spec/triangle_spec.rb
+++ spec/triangle_spec.rb
@@ -3,6 +3,11 @@ require File.expand_path(File.dirname(__FILE__) + '/../triangle')
 describe Triangle do
   specify { expect(Triangle.check_triangle(2, 3, 4)).to eq Triangle::SCALENE }
   specify { expect(Triangle.check_triangle(2, 2, 1)).to eq Triangle::ISOSCELES }
+  specify { expect(Triangle.check_triangle(3, 3, 4)).to eq Triangle::ISOSCELES }
+  specify { expect(Triangle.check_triangle(3, 4, 4)).to eq Triangle::ISOSCELES }
   specify { expect(Triangle.check_triangle(1, 1, 1)).to eq Triangle::EQUILATERAL }
+  specify { expect(Triangle.check_triangle(2, 2, 2)).to eq Triangle::EQUILATERAL }
   specify { expect(Triangle.check_triangle(1, 2, 3)).to eq Triangle::NOT_TRIANGLE }
+  specify { expect(Triangle.check_triangle(1, 1, 2)).to eq Triangle::NOT_TRIANGLE }
+  specify { expect(Triangle.check_triangle(2, 3, 5)).to eq Triangle::NOT_TRIANGLE }
 end
