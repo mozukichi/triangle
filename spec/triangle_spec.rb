4cd61e1a5b6008aa40b09c6544cec764ae59eb79
zukkun <zukkun@iwaboos-mba13.lan>
2014-10-19 15:01:04 +0900

    initial commit

---
+++ spec/triangle_spec.rb
@@ -0,0 +1,8 @@
+require File.expand_path(File.dirname(__FILE__) + '/../triangle')
+
+describe Triangle do
+  specify { expect(Triangle.check_triangle(2, 3, 4)).to eq Triangle::SCALENE }
+  specify { expect(Triangle.check_triangle(2, 2, 1)).to eq Triangle::ISOSCELES }
+  specify { expect(Triangle.check_triangle(1, 1, 1)).to eq Triangle::EQUILATERAL }
+  specify { expect(Triangle.check_triangle(1, 2, 3)).to eq Triangle::NOT_TRIANGLE }
+end
---
+++ triangle.rb
@@ -0,0 +1,31 @@
+class Triangle
+  EQUILATERAL = 0
+  ISOSCELES = 1
+  SCALENE = 2
+  NOT_TRIANGLE = 3
+
+  def self.check_triangle(e1, e2, e3)
+    if e1 < e2 + e3 && e2 < e1 + e3 && e3 < e1 + e2 then
+      if e1 == e2 && e2 == e3 then
+        EQUILATERAL
+      elsif e1 == e2 || e2 == e3 || e1 == e3 then
+        ISOSCELES
+      else
+        SCALENE
+      end
+    else
+      NOT_TRIANGLE
+    end
+  end
+end
+
+case Triangle.check_triangle($*[0].to_i, $*[1].to_i, $*[2].to_i)
+when Triangle::EQUILATERAL then
+  puts "正三角形ですね！"
+when Triangle::ISOSCELES then
+  puts "二等辺三角形ですね！"
+when Triangle::SCALENE then
+  puts "不等辺三角形ですね！"
+when Triangle::NOT_TRIANGLE then
+  puts "三角形じゃないです＞＜"
+end
